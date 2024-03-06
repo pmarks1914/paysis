a869b5e1be7ade95f733ae0d0a9dd61049aac9a1
Patrick <pmarks1914@gmail.com>
2024-03-06 23:46:32 +0000

    sms boiler template

---
+++ Notification/Sms/sendSms.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_mail import Mail, Message
+from dotenv import dotenv_values
+from Model import *
+
+get_env = dotenv_values(".env")  
+
+app = Flask(__name__)
+# db = SQLAlchemy(app)
+
+def send_notification_sms(to, subject):
+    with app.app_context():
+        try:
+            return True
+        except Exception as e:
+            print(f"Failed to send sms: {e}")
+            return False
