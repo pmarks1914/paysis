628b13dcc1e28fab41b166322214267798f255c5
Patrick <pmarks1914@gmail.com>
2024-04-04 22:37:58 +0000

    model for transaction by business fix

--- Model.py
+++ Model.py
@@ -380,7 +380,7 @@ class Transaction(db.Model):
             'pagination': pagination_data
         }
 
-    # get transacttions by business
+    # get transacttion by business
     def getTransactionByBusinesId(id, page=1, per_page=10): 
         pagination = Transaction.query.filter_by(apikey_id=id).paginate(page=page, per_page=per_page, error_out=False)
         # Extract the items for the current page
@@ -388,7 +388,12 @@ class Transaction(db.Model):
         # Render nested objects
         new_data_object = [alchemy_to_json(item) for item in new_data]
         # Prepare pagination information to be returned along with the data
-
+        pagination_data = {
+            'total': pagination.total,
+            'per_page': per_page,
+            'current_page': page,
+            'total_pages': pagination.pages
+        }
         return {
             'data': new_data_object,
             'pagination': pagination_data
